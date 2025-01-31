325beef60def42255143b3b5ed001c3726301be4
KalebCarrillo <kaleb.carrillo@ucas-edu.net>
2025-01-31 11:41:09 -0700

    Variables practice py

---
+++ Variables/Practice.py
@@ -0,0 +1,21 @@
+name = "Kaleb"
+
+Number1 = 5
+
+Number2 = 47
+
+Breakfast = "eggs"
+
+Color = "blue"
+
+School = "UCAS"
+
+Year = 2025
+
+EyeColor = "Brown"
+
+Age = "15"
+
+Subject = "math"
+
+print(name, "I am", Age, "my eye color is", EyeColor, "I like the color", Color, 'I had', Breakfast, "for breakfast", "I go to", School, "my favorite subject is", Subject, "I like the number", Number1, "And the number", Number2,)
