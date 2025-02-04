12c59189ab0bacd7789fc082004f529a57b6b97f
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-02-04 19:17:14 +0000

    did practice.c

---
+++ Variables/Practice.c
@@ -0,0 +1,17 @@
+//Kaleb Carrillo, C Practice
+
+#include <stdio.h>
+int main(void){
+   char name[]= "Kaleb";
+   int number1 = 2;
+   int number2 = 240;
+   char breakfast[] = "bacon and eggs";
+   char color[] = "blue";
+   char school[] = "UCAS";
+   int year = 2025;
+   char eye[] = "brown";
+   int age = 15;
+   char subject[] = "math";
+   printf("Hello World");
+   return 0;
+}
