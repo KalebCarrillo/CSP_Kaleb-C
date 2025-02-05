1f67194685c6c3b2edff00a5a2fefa560a6c8958
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-05 11:40:49 -0700

    practice c

--- Variables/Practice.c
+++ Variables/Practice.c
@@ -1,7 +1,27 @@
 // Kaleb Carrillo, Practice C
 #include <stdio.h>
 
+char name[] = "Kaleb";
+int num1 = 10;
+int num2 =240;
+char breakfast[] = "eggs";
+char color[] = "blue";
+char school[] = "UCAS";
+int year = 2025;
+char eye[] = "brown";
+int age = 15;
+char subject[] = "math";
+
 int main(void){
-    printf("Hello World");
+    printf("My name is %s.", name);
+    printf(" I am %d", age);
+    printf(" years old. I had %s", breakfast);
+    printf(" for breakfast. I like the color %s.", color);
+    printf(" My eye color is %s.", eye);
+    printf(" It is %d", year);
+    printf(" and I go to %s.", school);
+    printf(" My favorite subject is %s", subject);
+    printf(" I like the number %d", num1);
+    printf(" and the number %d.", num2);
     return 0;
 }
--- expressions/notes.c
+++ expressions/notes.c
@@ -4,9 +4,11 @@
 // + - * /
 int num = 12;
 float pi = 3.1415936535;
+float expression = 5*7/2;
 int main(void){
-    num = 4;
-    printf("%d\n", num);
-    printf("Hello World");
+    //num = 4;
+    //printf("%d\n", num);
+    //printf("Hello World");
+    //printf("%f", expression);
     return 0;
 }
