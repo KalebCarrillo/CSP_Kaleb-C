ae5ae8c6ba6899bf06486af8611bb676bd7f6913
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-06 11:49:29 -0700

    half of calc py

---
+++ expressions/financial_calculator.c
@@ -0,0 +1,8 @@
+// Kaleb Carrillo, Financial Calculator C
+
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World");
+    return 0;
+}
---
+++ expressions/financial_calculator.py
@@ -0,0 +1,39 @@
+# Kaleb Carrillo, Financial Calculator pythonS
+
+# print statment that welcomes user tells what the program does
+print("Welcomes user this is a fanacial calculator for your needs.")
+# ask what their income is (varible an input)
+income = float(input("What is your income?\n"))
+# ask what their rent is (varible an input)
+rent = float(input("What is your rent?\n"))
+# ask what their utlitites is (varible an input)
+utlitites = float(input("What is your utlitites cost?\n"))
+# ask what their groceries is (varible an input)
+groceries = float(input("What is groceries cost?\n"))
+# ask what their transportation is (varible an input)
+transportation = float(input("what is your transportation cost?"))
+# calculate savings as 10% of income (income*.1) (varible)
+income *= .1
+# calculate spending as income-savings-rent-utilites-groceries-transportaion (varible)
+
+# calculate percent income of rent (rent/income *100) (varible)
+
+# calculate percent income of utilites (utilites/income *100) (varible)
+
+# calculate percent income of groceries (groceries/income *100) (varible)
+
+# calculate percent income of transportation(transportation/income *100) (varible)
+
+# calculate percent income of spending (spending/income *100) (varible)
+
+# Your rent is $XX.XX which is XX% of your income. (Print)
+
+# Your utilites is $XX.XX which is XX% of your income. (Print)
+
+# Your groceries is $XX.XX which is XX% of your income. (Print)
+
+# Your transportation is $XX.XX which is XX% of your income. (Print)
+
+# Your saving is $XX.XX which is XX% of your income. (Print)
+
+# Your spending is $XX.XX which is XX% of your income. (Print)
