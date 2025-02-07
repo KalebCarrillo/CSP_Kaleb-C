43e429555cae2f375bf63d076ae78724c3438098
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-07 11:48:40 -0700

    calculator python

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -11,29 +11,29 @@ utlitites = float(input("What is your utlitites cost?\n"))
 # ask what their groceries is (varible an input)
 groceries = float(input("What is groceries cost?\n"))
 # ask what their transportation is (varible an input)
-transportation = float(input("what is your transportation cost?"))
+transportation = float(input("what is your transportation cost?\n"))
 # calculate savings as 10% of income (income*.1) (varible)
-income *= .1
+saving = income* .1
 # calculate spending as income-savings-rent-utilites-groceries-transportaion (varible)
-
-# calculate percent income of rent (rent/income *100) (varible)
-
+spendings = income - saving - rent - utlitites -groceries - transportation
 # calculate percent income of utilites (utilites/income *100) (varible)
-
+utlitite = (utlitites/income)*100
+# calc rent
+rents = (rent/income)*100
 # calculate percent income of groceries (groceries/income *100) (varible)
-
+grocerie = (groceries/income)*100
 # calculate percent income of transportation(transportation/income *100) (varible)
-
+transportations = (transportation/income)*100
 # calculate percent income of spending (spending/income *100) (varible)
-
-# Your rent is $XX.XX which is XX% of your income. (Print)
-
+spending = income*100
+# Your rent i s $XX.XX which is XX% of your income. (Print)
+print("your rent is $",rent, "Which is", rents, "% of your income. \n")
 # Your utilites is $XX.XX which is XX% of your income. (Print)
-
+print("your utlitites $", utlitites, "Which is", utlitite, "% of your income. \n")
 # Your groceries is $XX.XX which is XX% of your income. (Print)
-
+print("Your groceries $", groceries, "Which is", grocerie, "% of your income. \n")
 # Your transportation is $XX.XX which is XX% of your income. (Print)
-
+print("Your transportation $", transportation, "Which is", transportations, "% of your income. \n")
 # Your saving is $XX.XX which is XX% of your income. (Print)
-
+print("Your saving $", saving, "Whicj is",)
 # Your spending is $XX.XX which is XX% of your income. (Print)
