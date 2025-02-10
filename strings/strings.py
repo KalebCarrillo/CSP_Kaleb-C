29e98097438d574442b5e60c30c4b3c17c87bcea
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-10 11:49:14 -0700

    string notes python

---
+++ strings/strings.py
@@ -0,0 +1,9 @@
+# Kaleb Carrillo, Strings Notes Python
+
+#string is a data typr that holds any ifor surroundong by quotaion marks "" ''
+
+name = (input("What is your first name?\n").strip().capitalize())
+
+print(f"Hello {name} welcome to my perogram!")
+print("this is your name")
+sentence = " the quick brown fox jumps over the lazy dog"
