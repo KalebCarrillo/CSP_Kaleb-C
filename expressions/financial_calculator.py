cea2bc6d425d00be9efd3f103f1b390bc71ad62f
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-02-10 19:18:16 +0000

    calc python

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -12,8 +12,12 @@ utlitites = float(input("What is your utlitites cost?\n"))
 groceries = float(input("What is groceries cost?\n"))
 # ask what their transportation is (varible an input)
 transportation = float(input("what is your transportation cost?\n"))
+#spedning input
+Spendings3 = float
 # calculate savings as 10% of income (income*.1) (varible)
-saving = income* .1
+saving = income*.10
+#saving
+savings = income - rent - utlitites - groceries - transportation*.10
 # calculate spending as income-savings-rent-utilites-groceries-transportaion (varible)
 spendings = income - saving - rent - utlitites -groceries - transportation
 # calculate percent income of utilites (utilites/income *100) (varible)
@@ -25,7 +29,7 @@ grocerie = (groceries/income)*100
 # calculate percent income of transportation(transportation/income *100) (varible)
 transportations = (transportation/income)*100
 # calculate percent income of spending (spending/income *100) (varible)
-spending = income*100
+spending = (spendings/income)*100
 # Your rent i s $XX.XX which is XX% of your income. (Print)
 print("your rent is $",rent, "Which is", rents, "% of your income. \n")
 # Your utilites is $XX.XX which is XX% of your income. (Print)
@@ -35,5 +39,6 @@ print("Your groceries $", groceries, "Which is", grocerie, "% of your income. \n
 # Your transportation is $XX.XX which is XX% of your income. (Print)
 print("Your transportation $", transportation, "Which is", transportations, "% of your income. \n")
 # Your saving is $XX.XX which is XX% of your income. (Print)
-print("Your saving $", saving, "Whicj is",)
+print("Your spending $", spendings, "which is", spending, "% of your income. \n")
 # Your spending is $XX.XX which is XX% of your income. (Print)
+print("Your savings $", savings,"which is",saving, "% of your income. \n")
