79969a5cde4b0064a5cc6f8f7ee3beed3e542bb7
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-11 11:15:49 -0700

    string python

--- strings/strings.py
+++ strings/strings.py
@@ -2,8 +2,13 @@
 
 #string is a data typr that holds any ifor surroundong by quotaion marks "" ''
 
-name = (input("What is your first name?\n").strip().capitalize())
+#name = (input("What is your first name?\n").strip().capitalize())
 
-print(f"Hello {name} welcome to my perogram!")
-print("this is your name")
-sentence = " the quick brown fox jumps over the lazy dog"
+#print(f"Hello {name} welcome to my perogram!")
+#print("this is your name "+ name)
+sentence = "The quick brown fox jumps over the lazy dog."
+
+#print(len(sentence))
+#print(sentence[4])
+print(sentence.find("brown"))
+print(sentence[10:19])
