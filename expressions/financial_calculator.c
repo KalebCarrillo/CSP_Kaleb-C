dcc02e890dcfc1d4f35194238f866dd66efb4b9e
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-13 11:48:55 -0700

    calc c

--- expressions/financial_calculator.c
+++ expressions/financial_calculator.c
@@ -4,21 +4,45 @@
 
 float income;
 float rent;
-float utlitiles;
+float utilities;
 float groceries;
 float transportation;
 
 int main(void){
     printf("Welcomes user this is a fanacial calculator for your needs. \n");
     printf("how much is your in income: \n", income);
-    scanf("%f", income);
+    scanf("%f", &income);
     printf("How much is your rent: \n", rent);
-    scanf("%f", rent);
-    printf("How much are your utlitiles: \n", utlitiles);
-    scanf("%f", utlitiles);
+    scanf("%f", &rent);
+    printf("How much are your utlitiles: \n", utilities);
+    scanf("%f", &utilities);
     printf("How much is are your groceries: \n");
-    scanf("%f", groceries);
+    scanf("%f", &groceries);
     printf("How much is transportation: \n", transportation);
-    scanf("%f", transportation);
+    scanf("%f", &transportation);
+
+    float utilitie;
+    float rents;
+    float grocerie;
+    float transportations;
+    float saving;
+    float spending;
+
+    saving = (income*.1);
+   //calculate sav ing as income-savings-rent-utilites-groceries-transportaion (varible)
+    spending = income - saving - rent - utilities - groceries - transportation;
+
+    utilitie = (utilities/income)*100;
+
+    rents = (rent/income)*100;
+  //calculate percent income of groceries (groceries/income *100) (varible)
+    grocerie = (groceries/income)*100;
+  //calculate percent income of transportation(transportation/income *100) (varible)
+    transportations = (transportation/income)*100;
+  //calculate percent income of spending (spending/income *100) (varible)
+    //spending = (spend/income)*100;
+ 
+    printf("your rent is $ %.2f. Which is %.2f %% of your income. \n", rent, rents);
+    printf("Your utilities $ %.2f. Which is %.2f %% of your income. \n", utilities, utilitie);
      return 0;
 }
---
+++ strings/silly_sentences.c
@@ -0,0 +1,15 @@
+// Kaleb Carrillo, Silly Sentences C
+#include <stdio.h>
+// Empty string variables for ser words (minimum 3)
+
+
+int main(void){
+    // A welcome for the user telling them what the program is (print)
+
+    //ask user for words (print statemnt with a question scanf to set to variable) (in C we need to tell the user they can type 1 word)
+
+    //print out the story with the variable inserted ("wecome %s to my program", name)
+
+
+    return 0;
+}
