1988997cf3ffa689d345822be963ee5c9529865a
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-02-18 19:15:38 +0000

    silly sentence python

--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -1,10 +1,10 @@
 # Kaleb Carrillo, Silly Sentences Python
 
 def values(type):
-    return input(f"_Please give me a {type}:")
+    return input(f"Please give me a {type}:\n")
 
-values("name")
-values("place")
-values("verb")
+noun = values("noun")
+adjective = values("adjective")
+noun2 = values("noun")
 
-print(f"(name)")
+print("After hiding the painting in his",noun,"for two years, he grew",adjective,"and tried to sell it to a/an",noun2,"in Florence, but was chaught.")
