48aa479a20cb5b6ddf266f6b0b8698c7a8689bf5
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-19 11:49:06 -0700

    sillu sentences c

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -5,7 +5,7 @@
 
 int main(void){
     // A welcome for the user telling them what the program is (print)
-
+    printf("Hello user welcome to my silly sentence maker!");
     //ask user for words (print statemnt with a question scanf to set to variable) (in C we need to tell the user they can type 1 word)
 
     //print out the story with the variable inserted ("wecome %s to my program", name)
