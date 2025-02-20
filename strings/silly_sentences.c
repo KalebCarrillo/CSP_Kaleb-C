fff8eedb50786526c339ed271be0ad15cb6790ad
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-20 11:49:19 -0700

    silly sentences c

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -3,11 +3,14 @@
 // Empty string variables for ser words (minimum 3)
 
 
-int main(void){
-    // A welcome for the user telling them what the program is (print)
-    printf("Hello user welcome to my silly sentence maker!");
-    //ask user for words (print statemnt with a question scanf to set to variable) (in C we need to tell the user they can type 1 word)
 
+int main(void){
+char name[50], place[50], verb[50];
+    printf("Hello user welcome to my silly sentence maker!\n");
+    //ask user for words (print statemnt with a question scanf to set to variable) (in C we need to tell the user they can type 1 word)
+    printf("Please give me a famous persons name: \n", name);
+    scanf("%s", name);
+    
     //print out the story with the variable inserted ("wecome %s to my program", name)
 
 
