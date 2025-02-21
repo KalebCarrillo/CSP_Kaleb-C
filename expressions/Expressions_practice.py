17b0e9625c166c621f7182b1fbed49c6704c0d21
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-21 11:49:45 -0700

    expressions practice python

--- expressions/Expressions_practice.py
+++ expressions/Expressions_practice.py
@@ -1,2 +1,31 @@
 # Kaleb Carrillo, Expressions Practice Python
 
+equation1 = 7-24/8*4+6
+
+equation2 = 18/3-7+2*5
+
+equation3 = 6*4/12+72/8-9
+
+equation4 = (17-6/2)+4*3
+
+equation5 = -2*(1*4-2/2)+(6+2-3)
+
+equation6 = -1*((3-4*7)/5)-2*24/6
+
+equation7 = (3* pow(5, 2) /15)-(5- pow(2, 2))
+
+equation8 = (pow(1, 4)*pow(2,2)+pow(3, 3))-pow(2, 5)/4
+
+equation9 = pow(22/2-2*5, 2)+pow(4-6/6,2)
+
+
+
+print(equation1)
+print(equation2)
+print(equation3)
+print(equation4)
+print(equation5)
+print(equation6)
+print(equation7)
+print(equation8)
+print(equation9)
