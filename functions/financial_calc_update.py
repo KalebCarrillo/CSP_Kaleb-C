7ef0a596264f25c560216195864eed68ae7ae67d
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-02-28 19:08:42 +0000

    calc update python

--- functions/financial_calc_update.py
+++ functions/financial_calc_update.py
@@ -1,31 +1,20 @@
 # Kaleb Carrillo, Financial Calculator Update python
 print("Welcomes user this is a fanacial calculator for your needs.")
 
-def values(type):
-    float(input(f"What is your {type}?\n"))
+def values(value):
+    float(input(f"What is your {value}?\n"))
 
 income = values("income")
 rent = values("rent")
 utlities = values("utlities")
 groceries = values("groceries")
 transportation = values("transportation")
-# print statment that welcomes user tells what the program does
-# ask what their income is (varible an input)
-#income = float(input("What is your income?\n"))
-# ask what their rent is (varible an input)
-#rent = float(input("What is your rent?\n"))
-# ask what their utlitites is (varible an input)
-#utlities = float(input("What is your utlitites cost?\n"))
-# ask what their groceries is (varible an input)
-#groceries = float(input("What is groceries cost?\n"))
-# ask what their transportation is (varible an input)
-#transportation = float(input("what is your transportation cost?\n"))
 # calculate savings as 10% of income (income*.1) (varible)
 savings = (income*.1)
 saving = (savings/income)*100
-# calculate sav ing as income-savings-rent-utilites-groceries-transportaion (varible)
 spending = income-savings-rent-utlities-groceries-transportation
 spendings =(spending/income)*100
+
 
 def info(cost, income, type):
     percent = (cost/income)*100
