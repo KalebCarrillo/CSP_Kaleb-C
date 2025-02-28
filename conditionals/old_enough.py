3f77cb1289b0cc53b51687447cddb6c95a01c707
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-02-28 20:44:12 +0000

    old enough python

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,1 +1,13 @@
 #Kaleb Carrillo, Old Enough Python
+age = 15
+
+if age >=18:
+    print("You are old enough to vote!\n")
+elif age >=16:
+    print("You are old enough to drive!\n")
+elif age >=15:
+    print("You are old enough to get a learners permit!\n")
+elif age >=5:
+    print("You are old enough to go to school!\n")
+else:
+    print("You are to young to go to school")
