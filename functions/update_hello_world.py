1a5e042d3a2db55b54cfae11de311ee8676c4b41
Kaleb.Carrillo <kaleb.carrillo@ucas-edu.net>
2025-02-28 11:10:35 -0700

    hello world update python

---
+++ functions/update_hello_world.py
@@ -0,0 +1,10 @@
+#Kaleb Carrillo, Update hello world Python
+
+def values(name):
+    return "Hello " + name + "!" 
+
+print(values(input("What is your name?:\n")))
+print(values(input("What is your name?:\n")))
+print(values(input("What is your name?:\n")))
+print(values(input("What is your name?:\n")))
+print(values(input("What is your name?:\n")))
