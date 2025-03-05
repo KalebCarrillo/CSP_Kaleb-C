379b380013b83cfecf0577882eb79ca6c1fb9a15
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-03-05 16:49:37 +0000

    time of day python

--- conditionals/time_of_day.py
+++ conditionals/time_of_day.py
@@ -1,14 +1,10 @@
 #Kaleb Carrillo, Time of Day Python
 
-time = 10
+time = 13
 
 if time <=12:
     print("Good moning!\n")
-elif time 13 >= 18:
+elif time <= 18:
     print("Good afternoon!\n")
-elif time >=15:
+elif time <= 24:
     print("Good evening!\n")
-elif time >=5:
-    print("You are old enough to go to school!\n")
-else:
-    print("You are to young to go to school")
