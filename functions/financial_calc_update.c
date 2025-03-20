adbe9831ad6bc72172c4b46cb4d6d6b1ddcd8866
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-03-20 18:10:47 +0000

    calc update

--- functions/financial_calc_update.c
+++ functions/financial_calc_update.c
@@ -3,9 +3,9 @@
 #include <math.h>
 
 char name [1000];
-float user_input(char*question){
+float user_input(char*info){
     float value;
-    printf("%s", question);
+    printf("%s", info);
     scanf("%f", &value);
     return value;
 }
@@ -14,13 +14,14 @@ void print_result(char*item_name, float num, float income){
     printf("%s, your monthly %s is $%.2f which is %.2f%% of your income!\n", name, item_name, num, percentage);
 }
 int main(void){
+    printf("Welcome user to my Financial calculator for your needs!\n");
     float income, rent, utilities, groceries, transportation, savings, spendings;
 
     income = user_input("What is your income?: \n");
     rent = user_input("What is your rent?: \n");
     utilities = user_input("How much do your utilities cost?: \n");
     groceries = user_input("How much do your groceries cost?: \n");
-    transportation = user_input("How much does transportation cost (gas, bus pass, Lyft, etc.)?: \n");
+    transportation = user_input("How much does transportation cost?: \n");
     
     savings = income*0.1;
     spendings = income-savings-rent-utilities-groceries-transportation;
