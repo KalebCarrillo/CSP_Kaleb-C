ceeabbd75e60266734b7e563411525be7c30d9d4
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-03-25 17:49:23 +0000

    random python

---
+++ Loops/random.py
@@ -0,0 +1,6 @@
+import random
+words=["software", "iconic", "dirtbikes","manager","pepsi", "explode", "investagator" , "exciting", "attitude","computer", "architect", "printer","publication", "evaluation", "rhinoceros", "pharoah","alligator",]
+
+for word in words:
+    print(word)
+
