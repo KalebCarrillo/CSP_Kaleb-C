6760f790f7c44315b1664ea6d270eaa189d45181
KalebCarrillo <kaleb.carrillo@ucas-edu.net>
2025-04-22 11:47:47 -0600

    java script notes

---
+++ Java_script/notes.js
@@ -0,0 +1,11 @@
+let images = []
+
+function hello(){
+    document.getElementByTd("title").innerHTML= "Hello World!"
+}
+function hover(){
+    document.getElementById("img").src = "https://static01.nyt.com/images/2020/03/09/sports/09nba-topteams1/merlin_170229057_ce4be847-c57c-41fc-9a4d-70008084dff7-articleLarge.jpg?quality=75&auto=webp&disable=upscale"
+}
+function hidden(){
+    document.getElementById("meme").style.display = "block "
+}
