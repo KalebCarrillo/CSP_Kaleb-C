1f45bfa98d295508819a28fb88fae5465fb1f7fb
KalebCarrillo <Kaleb.carrillo@ucas-edu.net>
2025-05-05 00:18:31 +0000

    final

---
+++ Internet_Finial/Final.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function change(){
+    document.getElementById("image").src = "https://i0.wp.com/www.brycecanyoncountry.com/wp-content/uploads/bryce-canyon-national-park_bestcamping2_5132d5d4.webp"
+}
