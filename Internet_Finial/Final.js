802c4a124dfafd24bfc6fd22e42faa95262ed0db
KalebCarrillo <kaleb.carrillo@ucas-edu.net>
2025-05-05 11:48:13 -0600

    final

--- Internet_Finial/Final.js
+++ Internet_Finial/Final.js
@@ -1,3 +1,17 @@
+let images = ["https://cdn.aarp.net/content/dam/aarp/travel/destinations/2021/07/1140-bryce-canyon-hero.imgcache.rev.web.1914.1100.jpg", "https://www.reneeroaming.com/wp-content/uploads/2022/06/Bryce-Canyon-Guide-Inspiration-Point-1.jpg","https://th-thumbnailer.cdn-si-edu.com/prCcoKOQiPkBi5QvIyWHEQVIltE=/fit-in/1600x0/https://tf-cmsv2-journeys-media.s3.amazonaws.com/filer/d2/52/d252f665-21c6-4daa-b163-8911ddd0d383/ut_bryce_hoodoos_sunset_ist_176984017.jpg"]
+let counter = 0
+
+function change(){
+    if (counter < images.length){
+        document.getElementById("image").src = images[counter]
+        counter += 1
+    }else{
+        counter = 0
+        document.getElementById("image").src = images[counter]
+    }
+
+}
+
 function myFunction() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
@@ -5,7 +19,4 @@ function myFunction() {
     } else {
       x.style.display = "none";
     }
-  }
-  function change(){
-    document.getElementById("image").src = "https://i0.wp.com/www.brycecanyoncountry.com/wp-content/uploads/bryce-canyon-national-park_bestcamping2_5132d5d4.webp"
   }
